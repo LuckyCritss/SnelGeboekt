4231f541bacbd8bfc68f02ed3cab2701f016b1b3
gregdc@webtrace.be <gregdc01>
2021-08-15 15:33:36 +0200

    styled the html somewhat and added the links to /login /home

---
+++ src/main/java/com/eindwerk/SnelGeboekt/Controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.eindwerk.SnelGeboekt.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String rootHandler(Principal principal) {
+        if (principal != null) {
+            return ("redirect:/home");
+        }
+        return ("login");
+    }
+}

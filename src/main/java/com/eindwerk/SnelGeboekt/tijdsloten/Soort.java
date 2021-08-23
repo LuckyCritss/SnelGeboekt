210b278bc6fdd729864c4e9ab0da647550aea514
Brecht Gemmel <your_email@whatever.com>
2021-08-23 15:39:30 +0200

    added tijdslots met tijdsblok en week klasses

---
+++ src/main/java/com/eindwerk/SnelGeboekt/tijdsloten/Soort.java
@@ -0,0 +1,9 @@
+package com.eindwerk.SnelGeboekt.tijdsloten;
+
+public class Soort {
+
+    String name = "consultatie 2 personen";
+    Integer blocks = 4;
+
+    Week schedule;
+}
---
+++ src/main/java/com/eindwerk/SnelGeboekt/tijdsloten/Week.java
@@ -0,0 +1,22 @@
+package com.eindwerk.SnelGeboekt.tijdsloten;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Week
+{
+
+
+
+    private Set<Tijdsblok> maandag = new HashSet();
+    private Set<Tijdsblok> dinsdag = new HashSet();
+    private Set<Tijdsblok> woensdag = new HashSet();
+    private Set<Tijdsblok> donderdag = new HashSet();
+    private Set<Tijdsblok> vrijdag = new HashSet();
+    private Set<Tijdsblok> zaterdag = new HashSet();
+    private Set<Tijdsblok> zondag = new HashSet();
+
+
+
+    }
+

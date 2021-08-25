e78066597916c86b3cd1855acc9f7a060e2324c8
Christy <christySermon@hotmail.com>
2021-08-25 05:35:53 +0200

    adjustment to list options

--- src/main/java/com/eindwerk/SnelGeboekt/Controllers/OptieController.java
+++ src/main/java/com/eindwerk/SnelGeboekt/Controllers/OptieController.java
@@ -14,7 +14,7 @@ import javax.validation.Valid;
 
 
 @Controller
-@RequestMapping("/lijstkeuzemogelijkheden")
+@RequestMapping("/keuzemogelijkheden")
 public class OptieController {
 
     private OptieService optieService;

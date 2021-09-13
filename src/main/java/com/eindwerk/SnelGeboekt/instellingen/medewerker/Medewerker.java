9cb1f3986178aa2ca9f775e55cc76a3c2b16a04a
Christy <christySermon@hotmail.com>
2021-09-13 08:58:57 +0200

    step 1 still problem

--- src/main/java/com/eindwerk/SnelGeboekt/instellingen/medewerker/Medewerker.java
+++ src/main/java/com/eindwerk/SnelGeboekt/instellingen/medewerker/Medewerker.java
@@ -1,11 +1,13 @@
 package com.eindwerk.SnelGeboekt.instellingen.medewerker;
 
+import com.eindwerk.SnelGeboekt.instellingen.tijdsloten.Tijdslot;
 import com.eindwerk.SnelGeboekt.organisatie.Organisatie;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 
 @Entity
@@ -14,10 +16,29 @@ import javax.validation.constraints.NotBlank;
 @Table(name="medewerker")
 public class Medewerker {
 
+        public enum MedewerkerLijst {
+                NVT("Niet van toepassing"), Med1("Medewerker 1");
+
+                private final String naamMedewerker;
+
+                MedewerkerLijst(String naamMedewerker) {
+                        this.naamMedewerker = naamMedewerker;
+                }
+
+
+
+                public String getDisplayName() {
+                        return naamMedewerker;
+                }
+        }
+
     
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private int id;
+
+        @NotNull(message = "value mismatch")
+        private MedewerkerLijst naamMedewerker = MedewerkerLijst.NVT;
 
         @NotBlank(message = "{org.blank}")
         private String naam;
